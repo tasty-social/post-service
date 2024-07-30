280abfc34c42010b5b815ee624fd066c56fd9104
trikhanhhk <nguyentrikhanh123hk@gmail.com>
2024-07-30 17:49:44 +0700

    update auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { PostsModule } from './posts/posts.module'
-import { ConfigModule } from '@nestjs/config'
+import { ConfigModule, ConfigService } from '@nestjs/config'
 import { MongooseModule } from '@nestjs/mongoose'
 import { APP_GUARD } from '@nestjs/core'
 import { JwtAuthGuard } from './auth/jwt.guard'
 import { RolesGuard } from './auth/roles.guard'
+import { PassportModule } from '@nestjs/passport'
+import { JwtModule } from '@nestjs/jwt'
 
 @Module({
   imports: [
@@ -15,7 +17,16 @@ import { RolesGuard } from './auth/roles.guard'
       envFilePath: ['.env']
     }),
     MongooseModule.forRoot(process.env.MONGO_URI),
-    PostsModule
+    PostsModule,
+    PassportModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: { expiresIn: '60m' }
+      }),
+      inject: [ConfigService]
+    })
   ],
   controllers: [AppController],
   providers: [
